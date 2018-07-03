afa3f992b3b4381a0a3c3e94c8db7108957d7cf6
Don Kelly <karfai@gmail.com>
2018-07-03 14:38:51 +0400

    Implements a basic StatusController for testing config

---
+++ app/controllers/StatusController.scala
@@ -0,0 +1,34 @@
+// Copyright (C) 2018 Don Kelly <karfai@gmail.com>
+
+// This file is part of Interlibr, a functional component of an
+// Internet of Rules (IoR).
+
+// ACKNOWLEDGEMENTS
+// Funds: Xalgorithms Foundation
+// Collaborators: Don Kelly, Joseph Potvin and Bill Olders.
+
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Affero General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public
+// License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.libs.json._
+
+@Singleton
+class StatusController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(Json.toJson(Map("status" -> "live")))
+  }
+}
